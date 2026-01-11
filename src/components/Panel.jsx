45efb624b83d278909fac618ad1eeda9d78771a6
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-11 14:37:47 +0100

    feat: added new chat part and open/close panel part

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -2,15 +2,37 @@ import React from 'react'
 
 export default function Panel() {
     return (
-        <aside className="h-screen flex flex-col gap-2 bg-teal-200 max-w-[250px] p-4">
-            <h2 className="font-semibold">Chats</h2>
+        <aside className="h-screen flex flex-col gap-4 bg-teal-200 max-w-[300px] p-4">
+
+            {/* OPEN/CLOSE PANEL */}
+            <div className="flex justify-between">
+
+                {/* OPEN OR CLOSE PANEL */}
+                <span>Fermer</span>
+
+                {/* SEARCH FORMER CHATS */}
+                <span>Recherche</span>
+            </div>
+
+            {/* NEW CHAT */}
             <div>
-                <h2>Titre</h2>
-                <h2>Titre</h2>
-                <h2>Titre</h2>
-                <h2>Titre</h2>
-                <h2>Titre</h2>
-                <h2>Titre</h2>
+                <button>
+                    <span>+</span>
+                    Nouveau chat
+                </button>
+            </div>
+
+            {/* CHATS BLOC*/}
+            <div>
+                <h2 className="font-semibold">Chats</h2>
+                <div>
+                    <h2>Titre</h2>
+                    <h2>Titre</h2>
+                    <h2>Titre</h2>
+                    <h2>Titre</h2>
+                    <h2>Titre</h2>
+                    <h2>Titre</h2>
+                </div>
             </div>
         </aside>
     )
