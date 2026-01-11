b4cf0336df866c2bbea456df2cabce63b14417ae
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-11 17:43:14 +0100

    feat: create togglePanel function + panel size is changed according to the togglePanel value + icons changed depending on togglePanel

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ImCross } from "react-icons/im";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaSearch } from "react-icons/fa";
 
 export default function Panel() {
+
+    // Menu is closed by default
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Open and close the panel
+    const togglePanel = () => {
+        setIsOpen(!isOpen)
+    };
+
+
     return (
-        <aside className="h-screen flex flex-col gap-4 bg-teal-200 max-w-[300px] p-4">
+        <aside className={`h-screen flex flex-col gap-4 bg-teal-200 p-4 ${isOpen ? "max-w-[300px]" : "max-w-[70px]"}`}>
 
             {/* OPEN/CLOSE PANEL */}
             <div className="flex justify-between items-center">
 
-                {/* OPEN OR CLOSE PANEL */}
-                <ImCross />
+                <button onClick={togglePanel}>
+                    {isOpen ? <ImCross /> : <GiHamburgerMenu />}
+                </button>
 
                 {/* SEARCH FORMER CHATS */}
-                <span>Recherche</span>
+                <button>
+                    <FaSearch className={isOpen ? "block" : "hidden"} />
+                </button>
             </div>
 
             {/* NEW CHAT */}
