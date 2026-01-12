bb09be11a993f66ef90fd92d5d1fc670cd423310
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-12 16:05:12 +0100

    feat: chat template added with style

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Chat() {
   return (
-    <main className="flex flex-1 justify-center p-4">
+    <main className="flex flex-col flex-1 items-center p-4">
       <h1>Titre du chat</h1>
+      <div className="flex flex-col max-w-[850px] gap-6">
+        <p className="self-start max-w-[70%] p-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, deserunt voluptas! Reiciendis, eos? Molestias animi nemo voluptatum omnis quia. Nemo libero ex beatae debitis ratione nihil vero error vel accusamus!</p>
+        <p className="self-end bg-[#003c57] max-w-[70%] text-white p-4 rounded-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quia aliquam temporibus omnis minima maiores at a, quam dolores accusamus placeat ullam sapiente accusantium libero veritatis vitae, quo impedit facere.</p>
+        <p className="self-start max-w-[70%] p-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, deserunt voluptas! Reiciendis, eos? Molestias animi nemo voluptatum omnis quia. Nemo libero ex beatae debitis ratione nihil vero error vel accusamus!</p>
+        <p className="self-end bg-[#003c57] max-w-[70%] text-white p-4 rounded-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quia aliquam temporibus omnis minima maiores at a, quam dolores accusamus placeat ullam sapiente accusantium libero veritatis vitae, quo impedit facere.</p>
+        <p className="self-start max-w-[70%] p-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, deserunt voluptas! Reiciendis, eos? Molestias animi nemo voluptatum omnis quia. Nemo libero ex beatae debitis ratione nihil vero error vel accusamus!</p>
+        <p className="self-end bg-[#003c57] max-w-[70%] text-white p-4 rounded-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quia aliquam temporibus omnis minima maiores at a, quam dolores accusamus placeat ullam sapiente accusantium libero veritatis vitae, quo impedit facere.</p>
+      </div>
     </main>
   )
 }
