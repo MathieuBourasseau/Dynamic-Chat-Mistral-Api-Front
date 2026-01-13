89f389c16c121c1dc6738becbaa3c72e9c33f295
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-13 11:49:28 +0100

    feat: create handleSubmit function

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,18 +4,37 @@ import { FaArrowUp } from "react-icons/fa";
 export default function Chat() {
 
   // Data simulation : 
-  const messages = [
+  const chatTest = [
     { sender: "ai", text: "Bonjour, comment puis-je vous aider aujourd'hui ? 😊" },
     { sender: "user", text: "Quelle météo fait-il à Clisson ?" },
     { sender: "ai", text: "Il fait aujourd'hui 10°C dans la ville de Clisson." },
     { sender: "user", text: "Va-t-il pleuvoir aujourd'hui ?" },
   ]
 
-  // Handling form messages 
+ // Define a state to get a simulation of chat by default 
+ const [messages, setMessages] = useState(chatTest);
 
-  const [message, setMessage] = useState('');
+ // Define a state to get an empty textarea by default
+  const [message, setMessage] = useState(''); 
 
+ // Update value in the textarea
+  const handleMessage = (e) => {
+    setMessage(e.target.value)
+  }
 
+  // Handle new message sent in the form 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the form event by default
+
+    if(message.trim() === "") return; // Prevent the user to send empty message
+
+    const newMessage = { sender: "user", text: message }; // Get the user message from the textarea
+    
+    setMessages([...messages, newMessage]); // Create a copy from existing array and add the new message 
+
+    setMessage(''); // Textarea is cleared of its content
+
+  }
 
   return (
     <main className="flex flex-col flex-1 h-screen items-center p-4">
@@ -45,6 +64,7 @@ export default function Chat() {
       {/* CHAT INPUT */}
       <form
         action=""
+        onSubmit={handleSubmit}
         className="max-w-[850px] h-[150px] w-full border-2 border-gray-200 p-4 rounded-lg shadow-sm">
         <div className="flex h-full">
 
@@ -54,10 +74,14 @@ export default function Chat() {
             placeholder="Ecrivez votre question."
             className="h-full flex-1 outline-none resize-none pt-0"
             value={message}
+            onChange={handleMessage}
           />
 
           {/* BUTTON TO SEND MESSAGE */}
-          <button className="self-end cursor-pointer bg-[#f8532a] p-2">
+          <button 
+            className="self-end cursor-pointer bg-[#f8532a] p-2"
+            type="submit"
+          >
             <FaArrowUp className="text-white" />
           </button>
         </div>
