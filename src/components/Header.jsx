9e8be548834723deb13b3f0e97d9a060932ef8e6
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-23 18:31:32 +0100

    feat: PanelButton imported in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
+import PanelButton from "./PanelButton";
 
 export default function Header() {
   return (
@@ -7,8 +8,10 @@ export default function Header() {
 
         {/* MOBILE NAV */}
         <nav 
-            className="flex items-center justify-between md:hidden">
-            <GiHamburgerMenu />
+            className="flex items-center justify-between lg:hidden">
+            <PanelButton
+            
+            />
             <h1 className="font-bold">Projet LLM</h1>
             <span>user data</span>
         </nav>
