8c89d94b216c50f6368f5d484fb54a545f93b09f
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-23 18:28:31 +0100

    feat: Mobile nav is Hidden for large screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header() {
   return (
-    <header>
+    <header className="p-4">
+
+        {/* MOBILE NAV */}
         <nav 
-            className="flex justify-between">
-            <p>Menu burger</p>
+            className="flex items-center justify-between md:hidden">
+            <GiHamburgerMenu />
             <h1 className="font-bold">Projet LLM</h1>
             <span>user data</span>
         </nav>
