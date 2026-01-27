2f85b06dea8e26b40d874e79d199caf026e69425
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-27 13:01:50 +0100

    feat: disconnection mobile added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,8 +29,9 @@ export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, us
         <header className="p-4">
 
             {/* MOBILE NAV */}
+
             <nav
-                className="flex items-center justify-between lg:hidden">
+                className="flex items-center justify-between relative lg:hidden">
                 <button
                     onClick={() => setIsOpen(!isOpen)}
                     className="cursor-pointer"
@@ -39,10 +40,47 @@ export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, us
                         <GiHamburgerMenu className="text-[#003C57]" />
                     )}
                 </button>
+
+                {/* CHAT TITLE */}
                 <h1 className="font-bold text-[#003c57]">{currentTitle}</h1>
+
+                 {/* SHOW USER AVATAR */}
+                {user.avatar ? (
+                    <span
+                        className="cursor-pointer w-10 h-10 rounded-full flex items-center justify-center border border-[#003C57] shadow-sm"
+                        onClick={() => setIsDisconnected(!isDisconnected)}
+                    >
+                        <img src={user.avatar} alt="Avatar de l'utilisateur" />
+                    </span>
+                ) : (
+                    <span
+                        className="cursor-pointer w-10 h-10 rounded-full font-bold flex items-center justify-center border-3 border-[#003C57] shadow-sm"
+                        onClick={() => setIsDisconnected(!isDisconnected)}
+                    >
+                        {user.username[0].toUpperCase()}
+                    </span>
+                )}
+
+                {/* DISCONNECTION MOBILE */}
+                <div
+                    className={
+                        `z-50 bg-[#003C57] p-2 absolute right-0 top-full rounded-lg text-white transition-all duration-300
+                        ${isDisconnected ? "translate-y-2 opacity-100 hover:bg-[#F8532A] pointer-events-auto" : "translate-y-0 opacity-0 pointer-events-none"}
+                        `}
+                >
+                    <button
+                        type="button"
+                        className="flex items-center gap-2 cursor-pointer"
+                        onClick={handleLogOut}
+                    >
+                        Déconnexion
+                        <span><CiLogout /></span>
+                    </button>
+                </div>
             </nav>
 
             {/* DESKTOP NAV */}
+
             <nav className="hidden lg:block lg:flex lg:justify-between lg:items-center lg:relative">
                 <h1 className="text-[#003c57]">Projet portfolio LLM</h1>
 
