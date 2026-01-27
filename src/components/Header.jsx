a1a276c41e3c8a783b49f9fbbfc3d072ba0d5801
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-27 11:57:52 +0100

    feat: disconnection desktop button style added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { CiLogout } from "react-icons/ci";
+import { useState } from "react";
 
 export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, user }) {
+
+    // --- DEFINE STATE ---
+    const [isDisconnected, setIsDisconnected] = useState(false);
+
 
     // Find the current chat 
     const currentChat = chatsList ? chatsList.find(c => c.id === currentChatId) : null;
@@ -39,12 +44,14 @@ export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, us
                 {user.avatar ? (
                     <span
                         className="cursor-pointer w-10 h-10 rounded-full flex items-center justify-center border border-[#003C57] shadow-sm"
+                        onClick={() => setIsDisconnected(!isDisconnected)}
                     >
-                        <img src={user.avatar} alt="Avatar de l'utilisateur"/>
+                        <img src={user.avatar} alt="Avatar de l'utilisateur" />
                     </span>
                 ) : (
                     <span
                         className="cursor-pointer w-10 h-10 rounded-full font-bold flex items-center justify-center border-3 border-[#003C57] shadow-sm"
+                        onClick={() => setIsDisconnected(!isDisconnected)}
                     >
                         {user.username[0].toUpperCase()}
                     </span>
@@ -52,7 +59,10 @@ export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, us
 
                 {/* DISCONNECTION DESKTOP */}
                 <div
-                    className="bg-[#003C57] p-2 absolute lg:right-0 lg:bottom-[-50px] rounded-lg text-white"
+                    className={
+                        `bg-[#003C57] p-2 absolute lg:right-0 lg:top-full rounded-lg text-white transition-all duration-300
+                        ${isDisconnected ? "translate-y-2 opacity-100 pointer-events-auto" : "translate-y-0 opacity-0 pointers-events-none"}
+                        `}
                 >
                     <button
                         className="flex items-center gap-2"
@@ -61,6 +71,8 @@ export default function Header({ isOpen, setIsOpen, currentChatId, chatsList, us
                         <span><CiLogout /></span>
                     </button>
                 </div>
+
+
             </nav>
         </header>
     )
