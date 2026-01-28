2ef2121ccdfb1b2ffbff29a289ac054897beadd7
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-28 12:23:16 +0100

    feat: resetPassword component created

---
+++ src/components/ResetPassword.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from "react-router-dom"
+import { useState } from "react"
+
+export default function ResetPassword() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
